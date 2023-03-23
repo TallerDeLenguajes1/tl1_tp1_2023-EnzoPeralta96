a2fa7076107b0594132e7b352b7b2a70374e8811
Peralta Enzo <enzash.k@gmail.com>
2023-03-23 19:09:06 -0300

    declaracion de numero y puntero, valores

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -1,6 +1,13 @@
 #include<stdio.h>
 
 int main(){
-    printf ("hola mundo!");
+    printf ("hola mundo!\n");
+    int numeroEntero = 5 , *puntNumero;
+    puntNumero = &numeroEntero;
+    printf("El contenido del puntero es:%d\n",*puntNumero);
+    printf("La direccion de memoria almacenada es:%p\n",puntNumero);
+    printf("La direccion de memoria de la variable numeroEntero es:%p\n",&numeroEntero);
+    printf("La direccion de memoria del puntero es:%p\n",&puntNumero);
+    printf("El tamaño de memoria utilizado por el puntero es:%d byte\n", sizeof(int));
     return 0;
 }
